f31a8933bff301823a5789be8f4318901f870e7f
ziedlassoued <zied.lassoued@outlook.de>
2021-11-13 16:45:08 +0100

    :sparkles: Submit search form on debounced input

--- components/searchElement.js
+++ components/searchElement.js
@@ -2,8 +2,15 @@ import { createElement } from '../lib/elements';
 import styles from './searchElement.module.css';
 
 export default function createSearchElement(onSubmit) {
+  let timeoutId;
   const textInput = createElement('input', {
     placeholder: 'Search for a character',
+    oninput: () => {
+      clearTimeout(timeoutId);
+      timeoutId = setTimeout(() => {
+        onSubmit(textInput.value);
+      });
+    },
   });
   const searchElement = createElement(
     'form',
